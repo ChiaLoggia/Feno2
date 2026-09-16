4e32c0e29d76f9126079dca3c007b6676bbee905
ChiaLoggia <chiaraloggia@outlook.com>
2026-09-16 02:55:12 -0300

    Add files via upload

---
+++ INTRODUCCION-EDITABLE.js
@@ -0,0 +1,11 @@
+/* EDITABLE: textos de las dos pantallas iniciales. */
+window.INTRO_CONTENT = {
+  "masterclass-jp": {
+    portadaTitulo: "Masterclass de Juan Pablo Paz",
+    portadaSubtitulo: "",
+    portadaTexto: "Queremos acompañarte a revisar los principales contenidos de esta masterclass. La actividad te llevará pocos minutos y, si alcanzás el puntaje de aprobación, podrás descargar tu certificado.",
+    evaluacionTitulo: "A continuación te invitamos a responder unas preguntas sobre el contenido de la masterclass",
+    evaluacionTexto: "Son 10 preguntas de opción múltiple. Te recomendamos responderlas después de mirar la clase con atención.",
+    evaluacionObjetivo: "El objetivo de esta actividad es acompañar la comprensión de los principales conceptos presentados por Juan Pablo Paz. La evaluación se aprueba con 4 respuestas correctas."
+  }
+};
